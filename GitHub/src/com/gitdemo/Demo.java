5db76c80bd4c574775f463977f325ee3ed8a0030
ShubhamWadichar <104187300+ShubhamWadichar@users.noreply.github.com>
2022-04-25 15:31:39 +0530

    Update Demo.java

--- GitHub/src/com/gitdemo/Demo.java
+++ GitHub/src/com/gitdemo/Demo.java
@@ -2,6 +2,10 @@ package com.gitdemo;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("Method m1");
+	}
 	public static void main(String[] args) {
 		System.out.println("welcome to git hub");
 	}
